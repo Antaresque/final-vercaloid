259ee23b7e9d5147e57b52cbf61f27eb8d9a2542
Antaresque <olekfer@yahoo.pl>
2023-09-26 21:35:25 +0200

    prayge 2

--- src/lib/server/data.ts
+++ src/lib/server/data.ts
@@ -2,10 +2,10 @@ import type { HintPermission, HintStatus } from "$lib/types";
 
 export let perms: HintPermission[] = [
     { status: 'LOCKED', hints: [
+        { done: true, hint: false },
         { done: false, hint: false },
-        { done: false, hint: false },
-        { done: false, hint: false },
-        { done: false, hint: false },
+        { done: true, hint: false },
+        { done: true, hint: false },
     ]},
     { status: 'LOCKED', hints: [
         { done: false, hint: false },
@@ -29,7 +29,7 @@ export let perms: HintPermission[] = [
 
 export const data = [
     { img: 'http://i.imgur.com/6IhplpS.jpg', hintsUrl: 'Untitled.mp3/delinquent-duo', resultUrl: 'Untitled.mp3/delinquent-duo-final', songInfo: [], hints: [
-        { name: "Family outing", hint: '', solution: 'Have a result of a sibling song with one of the siblings in the result' },
+        { name: "Family outing", hint: '', solution: 'Have a picture of two siblings together out on the town' },
         { name: "A mascot concert", hint: '', solution: 'Have a result for Miku with one of her solo songs' },
         { name: "A duo of glowsticks", hint: '', solution: 'Have at least two people together in a Virtual Show room' },
         { name: "Showtime is over", hint: '', solution: 'Have a picture of losing a Showtime Ruler show, with Failed' }
